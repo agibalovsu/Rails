160445bcbfc706dccd5eb13e0bb4d8eb131aed27
agibalovsu <agibalovsu@mail.ru>
2021-10-28 22:35:08 +0300

    add foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2021_10_26_214854) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false, null: false
+    t.integer "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -27,15 +29,19 @@ ActiveRecord::Schema.define(version: 2021_10_26_214854) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
+    t.integer "test_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
