7f1950502c0a0e93580a7b97e3aa5b46d457f743
agibalovsu <agibalovsu@mail.ru>
2021-12-18 11:38:12 +0300

    add devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   root to: "tests#index"
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  get :logout, to: 'sessions#destroy'
-
-  resources :users, only: :create
-  resources :sessions, only: :create
-
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shalow: true, except: :index
     end
@@ -24,5 +18,14 @@ Rails.application.routes.draw do
       get :result
     end
   end
-  
+
+  namespace :admin do
+    root 'tests#index'
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shalow: true, except: :index
+      end
+    end
+  end
 end
+
