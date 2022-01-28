db95eeb27d2f5d9e27c3e43c18f10a8e0adf224d
agibalovsu <agibalovsu@mail.ru>
2022-01-29 01:31:16 +0300

    add changes 3 to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@
 users = User.create!([
                          {first_name: 'admin',
                           last_name: 'administrator',
-                          email:'agibalovsy1@gmail.com',
-                          password: 'S12e01r19g88',
-                          password_confirmation: 'S12e01r19g88',
+                          email:'agibalovsu@mail.ru',
+                          password: 'azitromicin',
+                          password_confirmation: 'azitromicin',
                           type: 'Admin'}
                      ])
 
