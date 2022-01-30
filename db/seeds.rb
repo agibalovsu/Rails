41bfa06f20843081b847dce2e1afdec6dd70cb4c
agibalovsu <agibalovsu@mail.ru>
2022-01-30 18:27:20 +0300

    add changes to seeds.rb 377

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,125 +1,48 @@
-User.create(first_name: 'admin', last_name: 'administrator', email: 'agibalovsy@gmail.com', password: 'qwerty', password_confirmation: 'qwerty', type: 'Admin')
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Category.create(title: 'HTML & CSS')
-Category.create(title: 'Javascript')
-Category.create(title: 'Ruby')
-Category.create(title: 'Ruby on Rails')
+users = User.create!([
+                         {first_name: 'admin',
+                          last_name: 'administrator',
+                          email:'agibalovsy@gmail.com',
+                          password: 'qwerty',
+                          password_confirmation: 'qwerty',
+                          type: 'Admin'}
+                     ])
 
-Test.create(title: 'HTML & CSS Basic', level: 1, category_id: 8, author_id: 5)
-Test.create(title: 'HTML & CSS Pro', level: 2, category_id: 8, author_id: 5)
-Test.create(title: 'Javascript Basic', level: 2, category_id: 9, author_id: 5)
-Test.create(title: 'Javascript Pro', level: 3, category_id: 9, author_id: 5)
-Test.create(title: 'Ruby', level: 2, category_id: 10, author_id: 5)
-Test.create(title: 'Ruby On Rails', level: 4, category_id: 11, author_id: 5)
+categories = Category.create!([
+                                  {title: 'Frontend'},
+                                  {title: 'Backend'},
+                                  {title: 'Mobile'}
+                              ])
 
-Question.create(body: 'С помощью каких свойств CSS можно задать цвета фона в блоке?', test_id: 11)
-Question.create(body: 'Какой тег отвечает за инициализацию таблицы?', test_id: 11)
-Question.create(body: 'Какой тег используется для отображения нумерованного списка?', test_id: 11)
+tests = Test.create!([
+                         {title: 'Ruby', level: 2, category: categories[1], author: users[0]},
+                         {title: 'HTML', level: 1, category: categories[0], author: users[0]},
+                         {title: 'Python', level: 3, category: categories[1], author: users[0]},
+                         {title: 'Swift', level: 3, category: categories[2], author: users[0]},
+                         {title: 'JS', level: 2, category: categories[0], author: users[0]}
+                     ])
 
-Question.create(body: 'Для чего используется тег <canvas>', test_id: 12)
-Question.create(body: 'Что означает атрибут «poster» для тега <video>', test_id: 12)
-Question.create(body: 'Как создаются переменные в SASS', test_id: 12)
+questions = Question.create!([
+                                 {body: 'Кто такой Matz?', test: tests[0]},
+                                 {body: 'Что такое HTML?', test: tests[1]},
+                                 {body: 'Кто такой Gvido?', test: tests[2]},
+                                 {body: 'Swift это ЯП под Android или под iOS?', test: tests[3]},
+                                 {body: 'Что такое Callback hell?', test: tests[4]}
+                             ])
 
-Question.create(body: 'Как узнать длину строки a?', test_id: 13)
-Question.create(body: 'Как при помощи jQuery получить элемент с ID my-element?', test_id: 13)
-Question.create(body: 'Javascript это диалект Java для клиента?', test_id: 13)
-
-Question.create(body: 'Выберите допустимые способы записи числа в JavaScript:', test_id: 14)
-Question.create(body: 'Что содержит объект даты, созданный с помощью инструкции “new Date()”?', test_id: 14)
-Question.create(body: 'Какими способами можно создать новый пустой объект?', test_id: 14)
-
-Question.create(body: 'Что возвращает следующее выражение? <code>[1, 2, 3, 4, 5].reject { |x| x.even? }.map { |x| x * x }</code>', test_id: 15)
-Question.create(body: 'Что будет возвращено следующим выражением? <code>[1, 2, 3] | [2, 3, 4]</code>', test_id: 15)
-Question.create(body: 'Что возвращает следующее выражение? <code>[1, 2, 3, 4, 5].select(&:odd?)</code>', test_id: 15)
-
-Question.create(body: 'В какой директории ROR приложения лежит основной код?', test_id: 16)
-Question.create(body: 'Как создать новое ROR приложение?', test_id: 16)
-Question.create(body: "Что вернет выражение? <code>['abc', 'def', 'gnt'].sort!</code>", test_id: 16)
-
-Answer.create(body: 'bk-color', correct: false, question_id: 150)
-Answer.create(body: 'background-color', correct: true, question_id: 150)
-Answer.create(body: 'color', correct: false, question_id: 150)
-Answer.create(body: 'background', correct: true, question_id: 150)
-
-Answer.create(body: '<div>', correct: false, question_id: 151)
-Answer.create(body: '<p>', correct: false, question_id: 151)
-Answer.create(body: '<span>', correct: false, question_id: 151)
-Answer.create(body: '<table>', correct: true, question_id: 151)
-
-Answer.create(body: '<ul>', correct: false, question_id: 152)
-Answer.create(body: '<list>', correct: false, question_id: 152)
-Answer.create(body: '<nlist>', correct: false, question_id: 152)
-Answer.create(body: '<ol>', correct: true, question_id: 152)
-
-Answer.create(body: 'Создает список вариантов, которые можно выбирать при наборе в текстовом поле', correct: false, question_id: 153)
-Answer.create(body: 'Создает область, в которой при помощи JavaScript можно рисовать разные объекты, выводить изображения', correct: true, question_id: 153)
-Answer.create(body: 'Используется для редактирования шрифта текста', correct: false, question_id: 153)
-
-Answer.create(body: 'При завершении композиции она запустится вновь', correct: false, question_id: 154)
-Answer.create(body: 'Отсутствие автоматической загрузки видеофайла', correct: false, question_id: 154)
-Answer.create(body: 'Выставляет изображение которое браузер будет использовать пока загружается', correct: true, question_id: 154)
-Answer.create(body: 'Добавление аудиодорожки', correct: false, question_id: 154)
-
-Answer.create(body: '&name', correct: false, question_id: 155)
-Answer.create(body: '@name', correct: false, question_id: 155)
-Answer.create(body: '$name', correct: true, question_id: 155)
-Answer.create(body: '#name', correct: false, question_id: 155)
-
-Answer.create(body: 'count(length)', correct: false, question_id: 156)
-Answer.create(body: 'a.length', correct: true, question_id: 156)
-Answer.create(body: 'Нет правильных вариантов', correct: false, question_id: 156)
-Answer.create(body: 'strlen(a)', correct: false, question_id: 156)
-
-Answer.create(body: '$("my-element")', correct: false, question_id: 157)
-Answer.create(body: '$(".my-element")', correct: false, question_id: 157)
-Answer.create(body: '$("#my-element")', correct: true, question_id: 157)
-Answer.create(body: 'document.get("#my-element")', correct: false, question_id: 157)
-
-Answer.create(body: 'Да', correct: false, question_id: 158)
-Answer.create(body: 'Нет', correct: true, question_id: 158)
-
-Answer.create(body: '015', correct: true, question_id: 159)
-Answer.create(body: '5e-2', correct: true, question_id: 159)
-Answer.create(body: '0x55', correct: true, question_id: 159)
-Answer.create(body: '0b1001', correct: true, question_id: 159)
-
-Answer.create(body: 'Текущую дату и время на начало дня', correct: false, question_id: 160)
-Answer.create(body: 'Пустую дату', correct: false, question_id: 160)
-Answer.create(body: 'Текущую дату и текущее время', correct: true, question_id: 160)
-
-Answer.create(body: 'var obj = {}', correct: true, question_id: 161)
-Answer.create(body: 'var obj = new {}', correct: false, question_id: 161)
-Answer.create(body: 'var obj = new Object()', correct: true, question_id: 161)
-Answer.create(body: 'var obj = []', correct: false, question_id: 161)
-
-Answer.create(body: "NoMethodError: undefined method 'map'", correct: false, question_id: 162)
-Answer.create(body: '[4, 16]', correct: false, question_id: 162)
-Answer.create(body: '[1, 9, 25]', correct: true, question_id: 162)
-
-Answer.create(body: '[1, 2, 3, 4]', correct: true, question_id: 163)
-Answer.create(body: '[1, 4]', correct: false, question_id: 163)
-Answer.create(body: '[2, 3, 4]', correct: false, question_id: 163)
-Answer.create(body: '[1, 2, 3, 2, 3, 4]', correct: false, question_id: 163)
-Answer.create(body: '[1, 2, 3, [2, 3, 4]]', correct: false, question_id: 163)
-
-Answer.create(body: '[2, 4]', correct: false, question_id: 164)
-Answer.create(body: '[1, 2, 3, 4, 5]', correct: false, question_id: 164)
-Answer.create(body: '[1, 3, 5]', correct: true, question_id: 164)
-Answer.create(body: "NoMethodError: undefined method 'select'", correct: false, question_id: 164)
-Answer.create(body: 'nil', correct: false, question_id: 164)
-
-Answer.create(body: 'bin/', correct: false, question_id: 165)
-Answer.create(body: 'doc/', correct: false, question_id: 165)
-Answer.create(body: 'app/', correct: true, question_id: 165)
-Answer.create(body: 'log/', correct: false, question_id: 165)
-
-Answer.create(body: 'ruby on rails new app first_app', correct: false, question_id: 166)
-Answer.create(body: 'ROR magic', correct: false, question_id: 166)
-Answer.create(body: 'Create application with ROR', correct: false, question_id: 166)
-Answer.create(body: 'rails new first_app', correct: true, question_id: 166)
-
-Answer.create(body: "['abc', 'def', 'gnt']", correct: true, question_id: 167)
-Answer.create(body: "{'abc', 'def', 'gnt'}", correct: false, question_id: 167)
-Answer.create(body: 'undefined', correct: false, question_id: 167)
-Answer.create(body: 'error', correct: false, question_id: 167)
-Answer.create(body: 'nil', correct: false, question_id: 167)
+answers = Answer.create!([
+                             {body: 'Создатель языка Руби', correct: true, question: questions[0]},
+                             {body: 'Создатель языка Пайтон', correct: false, question: questions[0]},
+                             {body: 'Создатель яыка JS', correct: false, question: questions[0]},
+                             {body: 'Создатель языка PHP', correct: false, question: questions[0]},
+                             {body: 'HyperText Markup Language', correct: true, question: questions[1]},
+                             {body: 'Hype Turbo Multi Language', correct: false, question: questions[1]},
+                             {body: 'Создатель языка Пайтон', correct: true, question: questions[2]},
+                             {body: 'Создатель языка PHP', correct: false, question: questions[2]},
+                             {body: 'Под iOS', correct: true, question: questions[3]},
+                             {body: 'Под Android', correct: false, question: questions[3]},
+                             {body: 'В JS есть второй псевдоним - лапша', correct: true, question: questions[4]},
+                             {body: 'Это такая игрушка', correct: false, question: questions[4]}
+                         ])
