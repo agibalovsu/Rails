d39762576dc9effffd842f6ddfdb1ea610eadb3b
agibalovsu <agibalovsu@mail.ru>
2022-02-21 22:29:10 +0300

    fix error

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,20 +1,20 @@
 class Admin::BadgesController < Admin::BaseController
-	
-	before_action :find_badge, only: %i[show edit update destroy]
+  
+  before_action :find_badge, only: %i[show edit update destroy]
 
-	def index
-		@badges = Badge.all
-	end
+  def index
+    @badges = Badge.all
+  end
 
-	def show; end
+  def show; end
 
-	def new
-		@badge = Badge.new
-	end
+  def new
+    @badge = Badge.new
+  end
 
-	def edit; end
+  def edit; end
 
-	def create
+  def create
     @badge = Badge.new(badge_params)
     if @badge.save
       redirect_to [:admin, @badge], notice: t('.success')
@@ -23,27 +23,27 @@ class Admin::BadgesController < Admin::BaseController
     end
   end
 
-	def update
-		if @badge.update(badge_params)
-			redirect_to [:admin, @badge]
-		else
-			render :edit
-		end
-	end
+  def update
+    if @badge.update(badge_params)
+      redirect_to [:admin, @badge]
+    else
+      render :edit
+    end
+  end
 
-	def destroy
-		@badge.destroy
-		redirect_to admin_badges_path
-	end
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
 
-	private
+  private
 
-	def find_badge
-		@badge = Badge.find(params[:id])
-	end
+  def find_badge
+    @badge = Badge.find(params[:id])
+  end
 
-	def badge_params
-		params.require(:badge).permit(:name, :image, :rule, :parameter)
-	end
+  def badge_params
+    params.require(:badge).permit(:name, :image, :rule, :parameter)
+  end
 end
 
--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,13 +1,13 @@
 module BadgesHelper
-	def first_try
-		Badge::Rules[0]
-	end
+  def first_try
+    Badge::Rules[0]
+  end
 
-	def all_level
-		Badge::Rules[1]
-	end
+  def all_level
+    Badge::Rules[1]
+  end
 
-	def all_in_category
-		Badge::Rules[2]
-	end
+  def all_in_category
+    Badge::Rules[2]
+  end
 end
